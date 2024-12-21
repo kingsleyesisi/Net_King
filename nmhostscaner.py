4e92f921a52594e02d1f9e6c7f5e72d365678dfc
KINGSLEY ESISI <kingsleyesisi@outlook.com>
2024-12-21 13:41:28 +0100

    update scan_ip function to change default port range to 5000 and improve documentation

--- nmhostscaner.py
+++ nmhostscaner.py
@@ -20,17 +20,22 @@ def check_os():
 
 UserOS = check_os()
 
-def scan_ip(base_ip, port_range: str = '2000'):
+def scan_ip(base_ip, port_range: str = '5000'):
     """ 
-    This function allows you to scan a range of IP addresses
-    and check the status of the ports on the IP addresses.
-    
-    it takes in one argument which is the base Ip address e.g. (192.168)
-    and it completes the rest 
+    It takes in one argument which is the base IP address (e.g., 192.168.)
+    and completes the rest of the IP address range to scan.
 
+    Args:
+        base_ip(str): The base IP address to scan (e.g., '192.168').
+        port_range (str, optional): The range of ports to scan. Defaults to '5000'.
+    Returns:
+        None
+        Results are printed to the console.
+    Example:
+        scan_ip('192.168')
+        This will scan all IP addresses from 192.168.0.0 to 192.168.255.255
+        and check the status of the ports in the specified range.
     """
-
-    nm = nmap.PortScanner()
     last_numb = [i for i in range(0, 256)]
     Third_numb = range(0, 256)
 
@@ -39,25 +44,28 @@ def scan_ip(base_ip, port_range: str = '2000'):
         for Last in last_numb:
             GateWay = f'{base_ip}.{Third}.{Last}'
             
-            # Scanning base on the port range so any port can be used 
-            # based on the user input
-
+            # Scanning for open ports
+        try:
+            nm = nmap.PortScanner()
             nm.scan(GateWay, port_range) 
             if nm[GateWay].state() == 'up':
-                print(f'Host : {GateWay} ({nm[GateWay].hostname()}) is up')
+                pass
+                # print(f'Host : {GateWay} ({nm[GateWay].hostname()}) is up')
 
             for proto in nm[GateWay].all_protocols():
                 lport = nm[GateWay][proto].keys()
                 for port in lport:
-                    print(f'Host : {GateWay} ({nm[GateWay].hostname()})')
-                    print(f'Port : {port}\tState : {nm[GateWay][proto][port]["state"]}')
-
+                    pass
+                    # print(f'Host : {GateWay} ({nm[GateWay].hostname()})')
+                    # print(f'Port : {port}\tState : {nm[GateWay][proto][port]["state"]}')
+        except Exception as e:
+            print(e)
 
 # Check Run Time of the function 
 def run(base_ip):
     start_time = time.time()
     scan_ip(base_ip)
     end_time = time.time()
-    print(f'Runtime: {end_time - start_time} seconds')
+    # print(f'Runtime: {end_time - start_time} seconds')
 
 run('192.168')
