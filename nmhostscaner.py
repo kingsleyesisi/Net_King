a6c1f4f18f3a66de72e36538dfc29fcb106f2a94
KINGSLEY ESISI <kingsleyesisi@outlook.com>
2024-12-21 13:45:27 +0100

    added clear documentation to the check_os function

--- nmhostscaner.py
+++ nmhostscaner.py
@@ -9,6 +9,17 @@ import time
 def check_os():
     """ 
     Checking the operating system of the User for smooth experience 
+
+    Args:
+        None
+    Returns:
+        str: The operating system of the user.
+    Example:
+        check_os()
+        This will return the operating system of the user.
+        => 'windows'
+        => 'linux'
+        => 'mac'
     """
 
     if os.name == 'nt':
